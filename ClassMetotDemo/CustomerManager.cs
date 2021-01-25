6272a16df6be50781e6df96475e94251a1906710
Gamze Yılmaz <gmzyilmaz7@gmail.com>
2021-01-25 04:37:48 +0300

    Tamamlandı

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine(customer.Id+" no'lu ID'ye sahip "+customer.Adi+" "+customer.Soyadi+" için hesap oluşturuldu.");
+        }
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " no'lu ID'ye sahip " + customer.Adi + " " + customer.Soyadi + " hesabı silinmiştir.");
+        }
+        public void CustomerList(Customer[] customers)
+        {
+            foreach (var i in customers)
+            {
+                Console.WriteLine("ID: "+i.Id + ", Adı Soyadı: " + i.Adi + " " + i.Soyadi);
+            }
+
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassMetotDemo
 {
@@ -6,7 +7,48 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Adi = "Recep";
+            customer1.Soyadi = "Şahin";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Adi = "Özge";
+            customer2.Soyadi = "Yılmaz";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Adi = "Elif";
+            customer3.Soyadi = "Polat";
+
+            Customer[] customers = new Customer[] {customer1,customer2,customer3};
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine("Ekleme Methodu Çalıştı.");
+            customerManager.CustomerAdd(customer1);
+            customerManager.CustomerAdd(customer2);
+            customerManager.CustomerAdd(customer3);
+
+            Console.WriteLine("Silme Methodu Çalıştı.");
+            customerManager.CustomerDelete(customer1);
+            customerManager.CustomerDelete(customer2);
+            customerManager.CustomerDelete(customer3);
+
+            Console.WriteLine("Listeleme Methodu Çalıştı.");
+            customerManager.CustomerList(customers);
+
+            //Console.WriteLine("Foreach ile listeleme yapıldı.");
+            //    foreach (var i in customers)
+            //    {
+            //        Console.WriteLine(i.Id + " no'lu ID'li " + i.Adi + " " + i.Soyadi);
+            //    }
+
+            
+            
+
         }
     }
 }
